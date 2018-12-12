bda9faf57e389777995b70f1e8ed2b80f809db76
caternuson <caternuson@gmail.com>
2018-12-12 11:48:07 -0800

    update example

--- examples/tcs34725_simpletest.py
+++ examples/tcs34725_simpletest.py
@@ -14,15 +14,9 @@ sensor = adafruit_tcs34725.TCS34725(i2c)
 
 # Main loop reading color and printing it every second.
 while True:
-    # Read the color as RGB bytes (0-255 values).
-    r, g, b = sensor.color_rgb_bytes
-    print('Detected color: #{0:02X}{1:02X}{2:02X}'.format(r, g, b))
     # Read the color temperature and lux of the sensor too.
-    try:
-        temp = sensor.temperature
-        lux = sensor.lux
-        print('Temperature: {0}K Lux: {1}'.format(temp, lux))
-    except ZeroDivisionError:
-        print("No light to measure")
+    temp = sensor.color_temperature
+    lux = sensor.lux
+    print('Temperature: {0}K Lux: {1}'.format(temp, lux))
     # Delay for a second and repeat.
     time.sleep(1.0)
