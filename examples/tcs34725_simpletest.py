6f4fb3af5ddb39ee4574359fca9fa57a13f060b9
Vi <silvia.copil@gmail.com>
2021-06-30 20:55:03 +0200

    hoping

--- examples/tcs34725_simpletest.py
+++ examples/tcs34725_simpletest.py
@@ -25,7 +25,11 @@ while True:
 
     color = sensor.color
     color_rgb = sensor.color_rgb_bytes
-    print("RGB color as 8 bits per channel int: #{0:02X} or as 3-tuple: {1}".format(color, color_rgb))
+    print(
+        "RGB color as 8 bits per channel int: #{0:02X} or as 3-tuple: {1}".format(
+            color, color_rgb
+        )
+    )
 
     # Read the color temperature and lux of the sensor too.
     temp = sensor.color_temperature
