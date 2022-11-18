9be362de73112e4ff959f14133d101a342d533c9
dherrada <dylan.herrada@adafruit.com>
2022-11-18 13:05:30 -0500

    Added commented out board.STEMMA_I2C with explanation

--- examples/tcs34725_simpletest.py
+++ examples/tcs34725_simpletest.py
@@ -10,6 +10,7 @@ import adafruit_tcs34725
 
 # Create sensor object, communicating over the board's default I2C bus
 i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 sensor = adafruit_tcs34725.TCS34725(i2c)
 
 # Change sensor integration time to values between 2.4 and 614.4 milliseconds
