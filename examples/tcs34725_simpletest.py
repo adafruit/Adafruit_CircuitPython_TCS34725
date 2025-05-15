308e5593b7d6b7c26499ec583bccfb52c609b51f
foamyguy <foamyguy@gmail.com>
2025-05-15 16:54:17 +0000

    change to ruff

--- examples/tcs34725_simpletest.py
+++ examples/tcs34725_simpletest.py
@@ -4,9 +4,10 @@
 # Simple demo of the TCS34725 color sensor.
 # Will detect the color from the sensor and print it out every second.
 import time
-import board
-import adafruit_tcs34725
 
+import board
+
+import adafruit_tcs34725
 
 # Create sensor object, communicating over the board's default I2C bus
 i2c = board.I2C()  # uses board.SCL and board.SDA
@@ -26,15 +27,11 @@ while True:
 
     color = sensor.color
     color_rgb = sensor.color_rgb_bytes
-    print(
-        "RGB color as 8 bits per channel int: #{0:02X} or as 3-tuple: {1}".format(
-            color, color_rgb
-        )
-    )
+    print(f"RGB color as 8 bits per channel int: #{color:02X} or as 3-tuple: {color_rgb}")
 
     # Read the color temperature and lux of the sensor too.
     temp = sensor.color_temperature
     lux = sensor.lux
-    print("Temperature: {0}K Lux: {1}\n".format(temp, lux))
+    print(f"Temperature: {temp}K Lux: {lux}\n")
     # Delay for a second and repeat.
     time.sleep(1.0)
